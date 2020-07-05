b182a554f74b87689b379cad1a2175b5f973dd41
snowjang24 <asdka4618@gmail.com>
2020-07-05 14:15:54 +0900

    end making GITHUB ACTION:

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
 <!-- 이 항목은 절대 건드리지 말 것(자동화) -->
 ## :file_folder: Table Of Contents
 <!-- toc starts -->
+## JAVASCRIPT
+
+* [Web 기본부터 Alaboza](https://github.com/Febase/FeBase/blob/master/Javascript/Web_Working_Concept.md) - 2020-07-02
+* [실행 컨텍스트와 클로저에 대해서](https://github.com/Febase/FeBase/blob/master/Javascript/Execution_Context_And_Closure.md) - 2020-07-02
+* [Garbage Collector](https://github.com/Febase/FeBase/blob/master/Javascript/Garbage_Collector.md) - 2020-07-02
+* [자바스크립트 동작 원리](https://github.com/Febase/FeBase/blob/master/Javascript/JS_Basic_movement.md) - 2020-06-25
+* [그냥 ES11인가 뭐시깽이 쓰면 되는거 아님?](https://github.com/Febase/FeBase/blob/master/Javascript/ES6_Spec.md) - 2020-06-25
+* [ES Module에 대해서](https://github.com/Febase/FeBase/blob/master/Javascript/ES_Module.md) - 2020-06-25
+* [따라쟁이 셋째 JS가 지키고 싶었던 것 - Prototype](https://github.com/Febase/FeBase/blob/master/Javascript/Prototype.md) - 2020-06-19
+* [비동기(Asynchronous)](https://github.com/Febase/FeBase/blob/master/Javascript/Asynchronous.md) - 2020-06-19
 <!-- toc ends -->
 
 ## HandBook
--- lib/rmd.py
+++ lib/rmd.py
@@ -8,10 +8,24 @@ class Rmd:
             r"<!\-\- toc starts \-\->.*<!\-\- toc ends \-\->", re.DOTALL)
         self.toc = ["<!-- toc starts -->"]
 
-    def update_toc(self, header, url):
-        print(header)
-        # row = "* [{title}]({url}) - {date}".format()
-        # self.toc.append(row)
+    def get_toc_row(self, toc):
+        header = toc['header']
+        date = re.search(r"([0-9]{4}\-[0-9]{2}\-[0-9]{2})", header['date'])
+        row = "* [{title}]({url}) - {date}".format(
+            title=header['title'],
+            url=toc['url'],
+            date=date.group()
+        )
+        return row
+
+    def update_toc(self, toc_data):
+        for category in toc_data.keys():
+            self.toc.append("## {}\n".format(category))
+            toc_list = sorted(toc_data[category],
+                              key=lambda toc: toc['header']['date'], reverse=True)
+            for toc in toc_list:
+                row = self.get_toc_row(toc)
+                self.toc.append(row)
 
     def rewrite(self):
         readme_contents = self.readme.open().read()
